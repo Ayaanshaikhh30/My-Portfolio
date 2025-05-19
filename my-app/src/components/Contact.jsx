038b01d9e5ae4dfe47c9179f8343df92a6758f98
Ayaanshaikhh30 <ayaanshaikhh30@gmail.com>
2025-05-19 19:55:44 +0530

    Changes

--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
 
     try {
       const response = await axios.post(
-        "https://portfolio-backend-3-cirm.onrender.com/api/users",
+        "https://portfolio-backend-3785.onrender.com/api/users",
         formData,
         {
           headers: { "Content-Type": "application/json" },
